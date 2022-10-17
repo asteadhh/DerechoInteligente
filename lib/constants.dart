4193b6dde39788c5fa23fdb9dc456fa701ebb121
asteadh <asteadh@hotmail.com>
2022-10-17 10:16:29 -0300

    initial

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,34 +1,4 @@
-import 'dart:ui';
-
-// Colors
-const Color kBlue = Color(0xFF306EFF);
-const Color kLightBlue = Color(0xFF4985FD);
-const Color kDarkBlue = Color(0xFF1046B3);
-const Color kWhite = Color(0xFFFFFFFF);
-const Color kGrey = Color(0xFFF4F5F7);
-const Color kBlack = Color.fromARGB(255, 67, 47, 45);
-
-// Padding
-const double kPaddingS = 8.0;
-const double kPaddingM = 16.0;
-const double kPaddingL = 32.0;
-
-// Spacing
-const double kSpaceS = 8.0;
-const double kSpaceM = 16.0;
-
-// Animation
-const Duration kButtonAnimationDuration = Duration(milliseconds: 600);
-const Duration kCardAnimationDuration = Duration(milliseconds: 400);
-const Duration kRippleAnimationDuration = Duration(milliseconds: 400);
-const Duration kLoginAnimationDuration = Duration(milliseconds: 1500);
-
-// Assets
-const String kGoogleLogoPath = 'assets/images/google_logo.png';
-const String kJurixLogoPath =
-    'assets/jurixlogo/300dpi/300dpi_jurix_isotipo_350px.png';
-
-// Dashboard
+import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,36 @@
-import 'firebase_options.dart';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-import 'app/config/routes/app_pages.dart';
-import 'app/config/themes/app_theme.dart';
-import 'initial_screen.dart';
+import '/controllers/MenuController.dart';
+import '/screens/main/main_screen.dart';
+import 'constants.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(App());
+void main() {
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // var size, height, width;
-    // size = MediaQuery.of(context).size;
-    // height = size.height;
-    // width = size.width;
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Pulpox Abogados',
-      theme: AppTheme.basic,
-      // initialRoute: AppPages.initial,
-      getPages: AppPages.routes,
-      // getPages: [
-      //   GetPage(name: '/page-three', page: () => const PageThree()),
-      //   GetPage(
-      //       name: '/page-four/:data',
-      //       page: () => const PageFour()) // Dynamic route
-      // ],
-      home:
-          // kIsWeb
-          //     ? Login(
-          //         screenHeight: MediaQuery.of(context).size.height,
-          //       )
-          //     :
-          InitialScreen(),
-
-      //#####ALTERNATIVE####
-      // Container(
-      //   child: Builder(
-      //     builder: (BuildContext context) {
-      //       final screenHeight = MediaQuery.of(context).size.height;
-
-      //       return Onboarding(
-      //         screenHeight: screenHeight,
-      //       );
-      //     },
-      //   ),
-      // ),
-      // home: InitialScreen(),
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
     );
   }
-}
-
-class CustomScrollBehaviour extends MaterialScrollBehavior {
-  @override
-  Set<PointerDeviceKind> get dragDevices => {
-        PointerDeviceKind.touch,
-        PointerDeviceKind.mouse,
-      };
 }
