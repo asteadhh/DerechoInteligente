d039e497e3a735381e5b0fc7e8ad88fc7d05a8ae
asteadh <asteadh@hotmail.com>
2022-10-24 17:52:15 -0300

    Upload indexstack platform

---
+++ lib/screens/dashboard/dashboardContent/dashboard_content.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../responsive.dart';
+import '../components/my_fields.dart';
+import '../components/storage_details.dart';
+
+class DashboardContent extends StatelessWidget {
+  const DashboardContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Column(
+            children: [
+              MyFiles(),
+              SizedBox(height: defaultPadding),
+              // RecentFiles(),
+              if (Responsive.isMobile(context))
+                SizedBox(height: defaultPadding),
+              if (Responsive.isMobile(context)) StarageDetails(),
+            ],
+          ),
+        ),
+        if (!Responsive.isMobile(context)) SizedBox(width: defaultPadding),
+        // On Mobile means if the screen is less than 850 we dont want to show it
+        if (!Responsive.isMobile(context))
+          Expanded(
+            flex: 2,
+            child: StarageDetails(),
+          ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/dashboard/pagos/pagos_content.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PagosContent extends StatelessWidget {
+  const PagosContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
