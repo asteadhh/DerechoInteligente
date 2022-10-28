4d5888fb6130be5de21af914bf114156d139beb6
asteadh <asteadh@hotmail.com>
2022-10-28 13:32:02 -0300

    set Auth Service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,8 +4,43 @@ class AuthService extends GetxService {
   Future<AuthService> init() async => this;
 
   final RxBool isAdmin = true.obs;
+  final RxBool chatSoporteAccess = true.obs;
+  final RxBool estadisticasAccess = true.obs;
+  final RxBool revenueAccess = true.obs;
+  final RxBool informacionUsuarioAccess = true.obs;
+  final RxBool creditosMaestrosAccess = true.obs;
+  final RxBool onBoardingMaestroAccess = true.obs;
+  final RxBool pagosAccess = true.obs;
 
-  void setIsPremium(bool newValue) {
+  void setIsAdmin(bool newValue) {
     isAdmin.value = newValue;
+  }
+
+  void setChatSoporteAccess(bool newValue) {
+    chatSoporteAccess.value = newValue;
+  }
+
+  void setStadisticasAccess(bool newValue) {
+    estadisticasAccess.value = newValue;
+  }
+
+  void setRevenueAccess(bool newValue) {
+    revenueAccess.value = newValue;
+  }
+
+  void setInformacionUsuarioAccess(bool newValue) {
+    informacionUsuarioAccess.value = newValue;
+  }
+
+  void setCreditosMaestrosAccess(bool newValue) {
+    creditosMaestrosAccess.value = newValue;
+  }
+
+  void setOnBoardingMaestroAccess(bool newValue) {
+    onBoardingMaestroAccess.value = newValue;
+  }
+
+  void setPagosAccess(bool newValue) {
+    pagosAccess.value = newValue;
   }
 }
