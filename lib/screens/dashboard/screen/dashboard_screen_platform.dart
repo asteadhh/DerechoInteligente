54695ef9ef60bf1a94e45914b0b0bece397f2e58
asteadh <asteadh@hotmail.com>
2022-10-28 13:04:28 -0300

    screens dashboard platform

--- lib/screens/dashboard/screen/dashboard_screen_platform.dart
+++ lib/screens/dashboard/screen/dashboard_screen_platform.dart
@@ -14,3 +14,87 @@ class PlatformAdminMainScreen extends StatelessWidget {
     );
   }
 }
+
+class ChatSoporteScreen extends StatelessWidget {
+  const ChatSoporteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: ChatSoporteScreen(),
+      templateName: 'Chat Soporte',
+    );
+  }
+}
+
+class EstadisticasScreen extends StatelessWidget {
+  const EstadisticasScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: EstadisticasScreen(),
+      templateName: 'Estadisticas',
+    );
+  }
+}
+
+class RevenueScreen extends StatelessWidget {
+  const RevenueScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: RevenueScreen(),
+      templateName: 'Revenue',
+    );
+  }
+}
+
+class InformacionUsuarioScreen extends StatelessWidget {
+  const InformacionUsuarioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: InformacionUsuarioScreen(),
+      templateName: 'Informacion Usuarios',
+    );
+  }
+}
+
+class CreditosMaestrosScreen extends StatelessWidget {
+  const CreditosMaestrosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: CreditosMaestrosScreen(),
+      templateName: 'Creditos Maestros',
+    );
+  }
+}
+
+class OnBoardingMaestrosScreen extends StatelessWidget {
+  const OnBoardingMaestrosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: CreditosMaestrosScreen(),
+      templateName: 'On Boarding Maestros',
+    );
+  }
+}
+
+class PagosScreen extends StatelessWidget {
+  const PagosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAdminTemplateScreen(
+      contentWidget: CreditosMaestrosScreen(),
+      templateName: 'Pagos',
+    );
+  }
+}
